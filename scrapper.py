9cb6a58a646a5b3b149f5bafb6d05466eef95dff
adhikarysamarth <adhikarysamarth@gmail.com>
2023-05-05 20:31:18 -0400

    Scraper code has been completed, and a test run has been successfully executed. The scrapper finished running and was able to extract 1000 records from the list.

--- scrapper.py
+++ scrapper.py
@@ -27,7 +27,7 @@ c.execute("CREATE TABLE IF NOT EXISTS Movies_1 (\
           Gross VARCHAR(10), \
           PRIMARY KEY (Title, Year))")
     
-# Create another table called Moves_2 with four columns    
+# Create another table called Movies_2 with four columns    
 c.execute("CREATE TABLE IF NOT EXISTS Movies_2 (\
           Title VARCHAR(50), \
           Year VARCHAR(4), \
@@ -35,3 +35,86 @@ c.execute("CREATE TABLE IF NOT EXISTS Movies_2 (\
           Top_250 VARCHAR(10))")
 
 
+# Create a dictionary with regex patterns to match and extract the content     
+regex_1 = {
+    'title': r"<a\s.*?\/title.*?>(.*?)</a>",
+    'year': r"<span\s.*\((....)\)</span>",
+    'movie_Rating': r"<span\s.*?certificate\">(.*?)</span>",
+    'runtime': r"<span\s.*?runtime\">(.*?)\smin</span>",
+    'genre': r"<span\s.*?genre\">\s*(.*?)\s*</span>",
+    'user_Rating': r"<div\s.*\s.*imdb.*\s.*<strong>(.*?)</strong>",
+    'metascore': r"<div\s.*metascore\">\s.*\">(.*?)\s.*\s.*\s.*</div>",
+    'director': r"Director:\s*<a[^>]*>([^<]+)</a>",
+    'actor_1': r"<a\s.*st_0\">(.*?)</a>",
+    'actor_2': r"<a\s.*st_1\">(.*?)</a>",
+    'actor_3': r"<a\s.*st_2\">(.*?)</a>",
+    'actor_4': r"<a\s.*st_3\">(.*?)</a>",
+    'user_Votes': r"<span class=\"text-muted\">Votes:</span>\s*<span name=\"nv\" data-value=\"([\d,]+)\">",
+    'gross': r"<span class=\"text-muted\">Gross:</span>\s*<span name=\"nv\" data-value=\"([\d,]+)\">"
+}
+
+# Create another dictionary with regex patterns
+regex_2 = {
+    'title': r"<a\s.*?\/title.*?>(.*?)</a>",
+    'year': r"<span\s.*\((....)\)</span>",
+    'plot': r"<p class=\"text-muted\">\s*(.*?)\s*</p>",
+    'top_250': r"chart/top/.*?>Top 250.*>#(.*?)</span>"
+}
+
+# Create a variable to store URL of the websote we are going to scrape, start=$NUM$ in the URL is where $NUM$ value is stored. $NUM starts with 1, and since one webpage has 50 movies listed, the next webpage starts from 51. So we will have to iterate through the pages 20 times.
+    
+URL = "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=$NUM$&ref_=adv_nxt"
+
+# To iterate 20 times, we will start with 1 and increase 50 after each page, we need to replace $NUM$ in the URL with 1, 51, 101,...and 951. 
+for i in range(1,1000,50):
+    new_URL = URL.replace("$NUM$", str(i))
+    print(new_URL) #to see which webpage we reached and to check the progress of the loop
+    driver.get(new_URL) # from selenium library; this will control our browser to interact with web pages we are crawling
+    page_content = driver.page_source # Extract HTML source code of the webpage. 
+    time.sleep(5) # To pause the iteration for five seconds
+    
+
+# Now we will get individual chunk from the list to iterate over it and extract required information.    
+    all_chunks = re.compile(r"<div class=\"lister-item-content\">(.*?)</p>\s*</div>\s*</div>",re.S|re.I).findall(page_content)
+    
+    # Iterate over all the chunks and match the regex patterns from regex_1, since re.findall results in a list of matches, we will store the list in result, and strip and store the first match into a dictionary named data
+    for chunk in all_chunks:
+        data = {}
+        
+        for key, regex in regex_1.items():
+            result = re.compile(regex, re.S|re.I).findall(chunk)
+            
+            if result:
+                data[key] = result[0].strip()
+            else:
+                data[key] = None
+                
+        title, year, movie_Rating, runtime, genre, user_Rating, metascore, director, actor_1, actor_2, actor_3, actor_4, user_Votes, gross = data.values() # Assign dictionary data into variables
+        
+        # Store the data into Movies_1 table
+        query = "INSERT INTO Movies_1 VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+        c.execute(query, (title, year, movie_Rating, runtime, genre, user_Rating, metascore, director, actor_1, actor_2, actor_3, actor_4, user_Votes, gross))
+    
+    # This is for extracting plot and top 250 columns. We will store this in table Movies_2    
+    for chunk in all_chunks:
+        data = {}
+        
+        for key, regex in regex_2.items():
+            result = re.compile(regex,re.S|re.I).findall(chunk)
+            
+            if result:
+                data[key] = result[0].strip()
+            else:
+                data[key] = None
+                
+        title, year, plot, top_250 = data.values()
+        
+        query = "INSERT INTO Movies_2 VALUES (?,?,?,?)"
+        c.execute(query, (title, year, plot, top_250))
+            
+    print(i)
+
+
+conn.commit()
+c.close()
+conn.close()
